0c220e23659892dcbbfc554e548116a35759046a
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-09 23:37:09 +0800

    Update topics.py

--- topics.py
+++ topics.py
@@ -2,7 +2,7 @@ from question import reed,transposing,clef,voice_types,piano_knowledge,orniments
 def pick_topic(pick = str):
     topic = {'Reed':reed(),'Transposing':transposing(),
              "Clef":clef(),"Voice types":voice_types(),
-             "Piano":piano_knowledge(),"Orniments":orniments(),
+             "Piano":piano_knowledge(),"Ornaments":orniments(),
              "Inst. technique":instrumental_technique()
              }
     return topic[pick]
