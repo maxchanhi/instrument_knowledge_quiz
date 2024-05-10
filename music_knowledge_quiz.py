cab795acc35fb397df33668c0276594111715373
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-10 17:14:21 +0800

    Update music_knowledge_quiz.py

--- music_knowledge_quiz.py
+++ music_knowledge_quiz.py
@@ -73,8 +73,10 @@ def main():
         if st.button('You can get an AI feedback'):
             with st.spinner("Generating feedback..."):
                 if st.secrets['Password'] == ss['userpd']:
+                    feedback_type = "Full feedback: "
                     feedback = provide_feedback(ss["student_ans"], 512)
                 else:
+                    feedback_type = "Limited feedback: "
                     feedback = provide_feedback(ss["student_ans"], 64)
             st.success(feedback)
             ss["student_ans"] = []
